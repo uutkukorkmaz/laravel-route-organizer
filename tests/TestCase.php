a843ad4f2274968a66a71504d75c271cca993777
uutkukorkmaz <uutkukorkmaz>
2022-04-23 14:57:32 +0300

    🔧 Fix - refresh testbench's app directory

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,10 @@
 
 namespace Uutkukorkmaz\RouteOrganizer\Tests;
 
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
+use http\Exception\InvalidArgumentException;
 use Illuminate\Foundation\Testing\Concerns\InteractsWithConsole;
 use Illuminate\Support\Facades\Artisan;
 use Orchestra\Testbench\TestCase as Orchestra;
@@ -25,6 +28,12 @@ abstract class TestCase extends Orchestra {
     protected function pathToTests(): string {
         return __DIR__;
     }
+
+    protected function setUp(): void {
+        parent::setUp();
+        File::deleteDirectory(app_path('Routes'));
+    }
+
 
     protected function assertRouteDoesExists(string $routeName): self {
         $this->assertTrue(Route::has($routeName));
